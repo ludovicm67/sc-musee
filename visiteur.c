c14681b01e59794a1b1a46200c4f51df1bb64b8e
ludovicm67 <ludovicmuller1@gmail.com>
2017-11-13 08:52:34 +0100

    use nanosleep

--- visiteur.c
+++ visiteur.c
@@ -1,17 +1,23 @@
 #include <stdlib.h>
-#include <unistd.h> // sleep
+#include <time.h>
 #include "musee.h"
 
+void temps_de_visite(int ms) {
+  struct timespec ts;
+  ts.tv_sec = (time_t) (ms / 1000);
+  ts.tv_nsec = (ms - ((long) ts.tv_sec * 1000)) * 1000000;
+  nanosleep(&ts, NULL);
+}
 
 int main(int argc, char * argv[]) {
-  int time;
+  int time, id;
   if (argc != 2) usage(argv[0], "time");
   time = atoi(argv[1]);
   if (time < 0) usage(argv[0], "time>=0");
   shm_acceder();
 
-  sleep(5);
-  int id = sem_acceder();
+  temps_de_visite(time);
+  id = sem_acceder();
   V(id);
 
   return EXIT_SUCCESS;
